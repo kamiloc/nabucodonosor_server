7774d0b9f7ce9b9c72c48f4ec6be25750dffe0ac
kamiloC <kamilo.sniper@gmail.com>
2020-03-28 04:42:39 -0500

    feat(docker): adding configuration files

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
-import {MONGODB_URLS} from "./config";
+import { MONGODB_CONFIG } from "./config";
 
 export async function connect() {
   try {
-    await mongoose.connect(MONGODB_URLS[0], {
+    await mongoose.connect(MONGODB_CONFIG[0].url, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      dbName: MONGODB_CONFIG[0].dbName
     });
-
-    console.log('>>> DB is conected');
+    console.log(">>> DataBase ID: "+MONGODB_CONFIG[0].dbName+" conected");
   } catch (e) {
-    console.log('Something goes wrong!');
+    console.log("Something goes wrong!");
     console.log(e);
   }
 }
