f5610e5fd0a0e825074f8fecdba682bc09fce41b
kamiloC <kamilo.sniper@gmail.com>
2020-04-18 03:55:24 -0500

    fix(database): adding a enviroment var to select db confing

--- src/database.js
+++ src/database.js
@@ -3,12 +3,12 @@ import { MONGODB_CONFIG } from "./config";
 
 export async function connect() {
   try {
-    await mongoose.connect(MONGODB_CONFIG[0].url, {
+    await mongoose.connect(MONGODB_CONFIG[process.env.MOVE_INDEX || 0].url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      dbName: MONGODB_CONFIG[0].dbName
+      dbName: MONGODB_CONFIG[1].dbName,
     });
-    console.log(">>> DataBase ID: "+MONGODB_CONFIG[0].dbName+" conected");
+    console.log(">>> DataBase ID: " + MONGODB_CONFIG[1].dbName + " conected");
   } catch (e) {
     console.log("Something goes wrong!");
     console.log(e);
