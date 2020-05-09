84abbffb555348b75102e0c3d3ac38c4802a3e22
kamiloC <kamilo.sniper@gmail.com>
2020-05-09 12:07:16 -0500

    fix: moving mqtt integration

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import express from "express";
 import graphqlHTTP from "express-graphql";
-import { connect as mqttConnect } from "mqtt";
 
 import schema from "./schema";
-import { MOVE_MAP, WS_URL } from "./config";
+import { MOVE_MAP } from "./config";
 import { connect } from "./database";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 connect();
-
-const mqttClient = mqttConnect(WS_URL);
-mqttClient.on("connect", () => {
-  console.log(">>> Mqtt connected succefull");
-});
 
 app.get("/", (_, res) => {
   res.json({
@@ -23,21 +17,6 @@ app.get("/", (_, res) => {
     version: "1.0",
     status: "working",
   });
-});
-
-app.post("/move", (_, res) => {
-  if (!mqttClient.connected) res.status(500).send("Mqtt client isn't connected");
-  else {
-    mqttClient.publish("move", MOVE_MAP[process.env.MOVE_INDEX || 0], (err) => {
-      if (err) {
-        console.log(">>> Error on publish topic");
-        console.log(err);
-        res.status(500).send({ response: err, message: "Move topic not published" });
-      } else {
-        res.status(200).send({ response: null, message: "Move topic published" });
-      }
-    });
-  }
 });
 
 app.use(
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,8 @@
-import Movement from "./models/movement";
 import GraphQLDateTime from "graphql-type-datetime";
+import { connect as mqttConnect } from "mqtt";
+
+import Movement from "./models/movement";
+import { MOVE_MAP, WS_URL } from "./config";
 
 export const resolvers = {
   DateTime: GraphQLDateTime,
@@ -7,13 +10,31 @@ export const resolvers = {
     async Movements() {
       const movements = await Movement.find();
       return movements;
-    }
+    },
   },
   Mutation: {
     async createMovement() {
-      const newMovement = new Movement({ status: 'created' });
+      const newMovement = new Movement({ status: "created" });
+      const mqttClient = mqttConnect(WS_URL);
+
+      mqttClient.on("connect", () => {
+        console.log(">>> Mqtt connected succefull");
+      });
+
+      if (!mqttClient.connected) res.status(500).send("Mqtt client isn't connected");
+      else {
+        mqttClient.publish("move", MOVE_MAP[process.env.MOVE_INDEX || 0], (err) => {
+          if (err) {
+            console.log(">>> Error on publish topic");
+            console.log(err);
+          } else {
+            console.log(">>> Message published");
+          }
+        });
+      }
+
       await newMovement.save();
       return newMovement;
-    }
-  }
-}
+    },
+  },
+};
